f8b9ab92775ddfdc42f8421e36aa507ab7b2429d
Yerzhanov Beksultan <200103513@stu.sdu.edu.kz>
2021-04-17 22:34:25 +0600

    First commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Go go go");
+        for(int i = 0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
